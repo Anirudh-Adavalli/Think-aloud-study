eb63c04b0492de29feaee68d4e5a3ffb7886c6ea
Hannes Breckenfelder <hannes.breckenfelder@gmx.de>
2021-01-26 11:51:49 +0100

    created main function to create array of objects, testing git

--- Ns,Ws,Obj/src/BicycleWheel.java
+++ Ns,Ws,Obj/src/BicycleWheel.java
@@ -1,4 +1,27 @@
+import static java.util.*;
 
 public class BicycleWheel {
 	float radiusInInch;
+	
+	public static void main(String args[]) {
+		BicycleWheel[] array = new BicycleWheel[4];
+		array[0].radiusInInch = 1;
+		array[1].radiusInInch = 3;
+		array[2].radiusInInch = 5;
+		array[3].radiusInInch = 7;
+		System.out.println(arrayAverage(array));
+	}
+	
+	static float arrayAverage(BicycleWheel[] array) {
+			int counter = 0;
+			float sum = 0;
+
+			while (counter < array.length) {
+				sum = sum + array[counter].radiusInInch;
+				counter = counter + 1;
+			}
+
+			float average = sum / (float) counter;
+			return average;
+	}
 }
